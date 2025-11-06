e7fca5f7409fa887168aa7e7830b9046a654ad4c
Brody Couture <brodycouture05@gmail.com>
2025-11-06 08:56:38 -0800

    finished exercise

--- bike.rb
+++ bike.rb
@@ -1,23 +1,8 @@
 # Bike
-
-class Bike
-
-  STANDARD_WEIGHT = 200 # lbs
+class Cargo
   MAX_CARGO_ITEMS = 10
-
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
-
-  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
-    @id = id
-    @color = color
-    @price = price
-    @weight = weight
-    @rented = rented
+  def initialize
     @cargo_contents = []
-  end
-
-  def rent!
-    self.rented = true
   end
 
   def add_cargo(item)
@@ -35,5 +20,26 @@ class Bike
   def pannier_remaining_capacity
     MAX_CARGO_ITEMS - self.cargo_contents.size
   end
-
 end
+
+class Bike < Cargo
+
+  STANDARD_WEIGHT = 200 # lbs
+
+  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
+
+  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
+    @id = id
+    @color = color
+    @price = price
+    @weight = weight
+    @rented = rented
+    @cargo_contents = []
+  end
+
+  def rent!
+    self.rented = true
+  end
+end
+
+
